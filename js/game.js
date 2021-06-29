03f5bf017ec291ed086287e01f6fe1f4642bbf4f
Caledy-M <melody.deckers@gmail.com>
2021-06-29 12:36:43 +0200

    html title + cat url

--- js/game.js
+++ js/game.js
@@ -265,7 +265,7 @@ function update() {
 
 function onClick(){
 //    window.open("https://www.google.com", "_blank");
-    window.location.replace("https://www.games.caramel.be/cat/index.html");
+    window.location.replace("https://games.caramel.be/cat/index.html");
 
 }
 
